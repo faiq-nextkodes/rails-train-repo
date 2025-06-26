74516b2f5a3570a68768b0dde7b923b817d75815
Faiq <your.faiq@nextkodes.com>
2025-06-26 14:13:12 +0500

    Init Commit 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  def welcome
+    render html: "Welcome"
+  end
   def hello
     render html: "Hello World"
   end
   def goodbye
-    render html: "Goodbye World"
+    render html: "This is a very big form"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "application#goodbye"
+  root "application#welcome"
+
+  get "/hello", to:"application#hello"
+
+  get "/goodbye", to:"application#goodbye"
+
 end
